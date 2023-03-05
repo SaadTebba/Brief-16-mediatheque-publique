ad33ca87b98a3b48f018b4ce87c19d76616debd7
Saad Tebba <116269919+SaadTebba@users.noreply.github.com>
2023-03-05 03:06:51 +0100

    ===

--- index.php
+++ index.php
@@ -10,8 +10,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -37,7 +36,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/165265fe22.js" crossorigin="anonymous"></script>
-    <script src="main.js"></script>
+    <script src="script.js"></script>
 </body>
 
 </html>
--- main.js
+++
@@ -1,6 +0,0 @@
-let button = document.getElementById("testi");
-
-button.onclick = function () {
-    console.log("Clicked");
-    alert("Clicked");
-}
